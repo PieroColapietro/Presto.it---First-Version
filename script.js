aa8ca7c4b9459dec398371e7ef4caa1d733fb857
Alessandro Di Lillo <sandro.dilillo@gmail.com>
2021-03-24 21:39:16 +0100

    tentativo di sistemazione carrot card 1

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function populateCategories() {
-    let categories = ['Motori', 'Telefonia', 'Immobili', 'Lavoro', 'Servizi', 'Elettronica', 'Accessori', 'Musica']
+    let categories = ['Motori', 'Telefonia', 'Immobili', 'Lavoro', 'Servizi', 'Elettronica', 'Accessori', 'Musica', 'pinco']
     let categoryWrapper = document.querySelector('#category-wrapper');
     categories.forEach(categoria => {
         let card = document.createElement('div')
-        card.classList.add('col-12', 'col-lg-3', 'd-flex')
+        card.classList.add('col-12', 'col-lg-4', 'py-3', )
         card.innerHTML = `
-                <div class="col-12 col-lg-3 d-flex">
-                    <div class="card card-custom">
+        <i class="fas fa-carrot card-carrot"></i>
+                      <div class="card card-custom">
                         <div class="card-body">
-                            
+                        <i class="fas fa-tshirt fs-1 pb-4"></i>
                             <h4 class="card-text fw-bold">${categoria}</h4>
                        
-                            <a href="#" class="btn btn-primary">Vai alla sezione</a>
+                            <a href="#" class="btn btn-primary btn-custom">Vai alla sezione</a>
                         </div>
                     </div>
                 </div>
